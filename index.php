44d2925b77fbb65627e98dfc188b535952afcb6d
Nguyễn Cao Kỳ <Caoky.nguyen.1704@gmail.com>
2020-11-16 12:14:08 +0700

    update jquery and css

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php ob_start(); session_start();  ?>
+<!DOCTYPE html>
+<html>
+<head>
+    <script src="js/jquery-3.5.1.min.js"></script>
+    <meta name="viewport" content="width=device-width,height=device-height,initial-scale=1.0"/>
+	<title>Shoping</title>
+	<link rel="stylesheet" href="css/bootstrap.min.css">
+    <link href="css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+    <script src="js/bootstrap.min.js"></script>
+    <link rel="stylesheet" type="text/css" href="css/StyleForPC.css">
+</head>
+<body>
+</body>
+</html>
+<?php ob_flush(); ?>
