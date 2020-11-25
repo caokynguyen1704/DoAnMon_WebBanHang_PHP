c51a0d9a0a5d10424ac4c0a7b843e8447b0aba61
Nguyễn Cao Kỳ <Caoky.nguyen.1704@gmail.com>
2020-11-25 23:45:11 +0700

    update login and logout

---
+++ api/login.php
@@ -0,0 +1,55 @@
+<div class="container dangnhap">
+    <center>
+        <h1>Đăng Nhập</h1>
+        <form method="POST">
+            <div class="input-group form-group">
+                <div class="input-group-prepend">
+                    <span class="input-group-text"><i class="fas fa-user"></i></span>
+                </div>
+                <input type="text" class="form-control" name="username" placeholder="Tên Đăng Nhập">
+            </div>
+            <div class="input-group form-group">
+                <div class="input-group-prepend">
+                    <span class="input-group-text"><i class="fas fa-key"></i></span>
+                </div>
+                <input type="password" class="form-control" name="password" placeholder="Mật Khẩu">
+            </div>
+            <div class="row align-items-center remember" style="color: black;">
+                <input type="checkbox">Ghi nhớ
+            </div>
+            <div class="form-group">
+                <input type="submit" value="Login" class="btn float-right login_btn">
+            </div>
+            <?php
+            if ( isset($_SESSION["error"]) ) {
+            echo('<div><p style="color:red">'.$_SESSION["error"]."</p></div>\n");
+            unset($_SESSION["error"]);
+            }
+            ?>
+        </form>
+    </center>
+</div>
+<?php
+        require "pdo.php";
+            if ((isset($_POST))&&(isset($_POST['username']))&&(isset($_POST['password']))){
+                $sql="select * from users where username= :user and password= :pass";
+                $stmt=$pdo->prepare($sql);
+                $stmt->execute(array(
+                    ':user'=>$_POST['username'],
+                    ':pass'=>$_POST['password']
+                ));
+                $rows=$stmt->fetchAll();
+                if(count($rows)==1){
+                    unset($_SESSION['name']);
+                    unset($_SESSION['user_id']);
+                    $_SESSION['name']=$rows[0]['username'];
+                    $_SESSION['user_id']=$rows[0]['id'];
+                    header("Location: ./index.php");
+                    return;
+                }else{
+                    $_SESSION["error"] = "Vui lòng nhập lại.";
+                    header( 'Location: ./index.php' ) ;
+                    return;
+                }
+            }
+?>
--- api/nav.php
+++ api/nav.php
@@ -13,18 +13,40 @@
     </div>
     <div class="col-2"></div>
     <div class="col-sm">
-<a src="#" onclick="if($('.cartorder').css('display')=='none') {$('.cartorder').show();$('.login').hide();}else{$('.cartorder').hide();}" class="btn btn-secondary"><i class="fa fa-shopping-cart" aria-hidden="true"></i></a>
-<a src="#" onclick="if($('.login').css('display')=='none'){$('.login').show();$('.cartorder').hide();}else{$('.login').hide();}" class="btn btn-secondary">Đăng Nhập</a>
+      
+<a src='#' onclick='btncart()' class='btn btn-secondary'><i class='fa fa-shopping-cart' aria-hidden='true'></i></a>
+<?php if (!(isset($_SESSION['user_id']))){
+        echo "<a src='#' onclick='btnlogin()' class='btn btn-secondary'>Đăng Nhập</a>";
+        }else{
+          echo "<a href='api/logout.php' class='btn btn-secondary'>Đăng Xuất</a>";
+        }
+?>
     </div>
   </div>
-    
+<script>
+  <?php 
+    if (isset($_SESSION["error"])){
+      echo "$(document).ready(function() { 
+        $('.login').show();
+        $('.cartorder').hide();
+       });";
+    }
+  ?>
+  function btncart(){
+    if($('.cartorder').css('display')=='none') {$('.cartorder').show();$('.login').hide();}else{$('.cartorder').hide();};
+  }
+  function btnlogin(){
+    if($('.login').css('display')=='none'){$('.login').show();$('.cartorder').hide();}else{$('.login').hide();}
+  }
+</script>    
 </nav>
 <div class="container-fluid cartorder" 
-  style="background-color: #cbcba9; ;display: none ;position:absolute; width:90%; height:80%; padding: 5px; left: 5%; box-shadow: 5px 5px 8px grey;">
+  style="position:absolute;z-index:2; background-color: #cbcba9; ;display: none ; width:90%; height:80%; padding: 5px; left: 5%; box-shadow: 5px 5px 8px grey;">
 This is cart
 </div>
 
 <div class="container-fluid login" 
-  style="background-color: #cbcba9; ;display: none ;position:absolute; width:90%; height:80%; padding: 5px; left: 5%; box-shadow: 5px 5px 8px grey;">
+  style="position:absolute; z-index:2 ;background-color: #cbcba9; ;display: none ; width:90%; height:80%; padding: 5px; left: 5%; box-shadow: 5px 5px 8px grey;">
 This is login
+<?php require 'login.php'; ?>
 </div>
